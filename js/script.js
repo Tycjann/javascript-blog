13c7f8159ed2df4181035f8271b624cefe0077a3
Mirosław Gucwa <mgucwa@gmail.com>
2022-03-12 10:49:41 +0100

    Del: class=active

--- js/script.js
+++ js/script.js
@@ -5,18 +5,33 @@
 //     console.log('links:', links);
 // });
 
-// tablica (lista?) wszystkich linków lewego menu (class = title) 
+// lista wszystkich linków lewego menu (class = title) 
 const links = document.querySelectorAll('.titles a');   
 
 // funkcja obsługująca kliknięcie w link lewego menu
 const titleClickHandler = function () { 
-    console.log(event);
+
+    // handler eventu otrzymywał argument, zawierający informacje na temat wychwyconego zdarzenia. 
+    // standardowa nazwa event
+    console.clear();
+    console.log(event, 'Link was clicked');
 
 // * remove class 'active' from all article links 
-
+const activeLinks = document.querySelectorAll('.titles a.active');
+for (const activeLink of activeLinks) {
+    // usuwamy z kadego linka class active
+    // classList zawiera informacje o wszystkich klasach tego elementu (opcje. remove, add, toggle, contains)
+    activeLink.classList.remove('active');
+}
 // * add class 'active' to the clicked link
 
 // * remove class 'active' from all articles
+const activeArticles = document.querySelectorAll('.posts article.active')
+{
+    for (const activeArticle of activeArticles) {
+        activeArticle.classList.remove('active');
+    }
+}
 
 // * get 'href' attribute from the clicked link
 
