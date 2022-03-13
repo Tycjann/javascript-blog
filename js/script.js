39084e977f8ca8e5783b269001883cf6f2385be1
Mirosław Gucwa <mgucwa@gmail.com>
2022-03-14 00:05:58 +0100

    Add: menu left - generated in JS

--- js/script.js
+++ js/script.js
@@ -1,74 +1,120 @@
+// document.getElementById('test-button').addEventListener('click', function(){
+//     const links = document.querySelectorAll('.titles a');
+//     console.log('links:', links);
+// });
+
 {
    'use strict';   // pokazuje wszystkie błedy w kodzie
+    console.clear();
 
-    // document.getElementById('test-button').addEventListener('click', function(){
-    //     const links = document.querySelectorAll('.titles a');
-    //     console.log('links:', links);
-    // });
+    const opt_all_article_selector = '.posts';
+    const opt_article_selector = '.post';
+    const opt_title_selector = '.post-title';
+    const opt_title_list_selector = '.titles';
 
-    // lista wszystkich linków lewego menu (class = title) 
-    const links = document.querySelectorAll('.titles a');   
+     
 
     // funkcja obsługująca kliknięcie w link lewego menu
     const titleClickHandler = function (event) 
     { 
         // handler eventu otrzymywał argument, zawierający informacje na temat wychwyconego zdarzenia. 
         // standardowa nazwa event
-        console.clear();
         // console.log(event);
 
         // blokowanie domyślengo zachowania przeglądarki po kliknięciu w linik
         event.preventDefault();
         
         // konkretny kliknięty link
-        const clickedElement = this;
+        const clicked_element = this;
 
         // * [DONE] remove class 'active' from all article links 
-        const activeLinks = document.querySelectorAll('.titles a.active');
-        for (const activeLink of activeLinks) {
+        const active_links = document.querySelectorAll(opt_title_list_selector + ' a.active');
+        for (const active_link of active_links) {
             // usuwamy z kadego linka class active
             // classList zawiera informacje o wszystkich klasach tego elementu (opcje. remove, add, toggle, contains)
-            activeLink.classList.remove('active');
+            active_link.classList.remove('active');
         }
         // * [DONE] add class 'active' to the clicked link
-        // console.log('clickedElement:', clickedElement);
-        clickedElement.classList.add('active');
+        // console.log('clicked_element:', clicked_element);
+        clicked_element.classList.add('active');
 
         // * [DONE] remove class 'active' from all articles
-        const activeArticles = document.querySelectorAll('.posts article.active')
+        const active_articles = document.querySelectorAll(opt_all_article_selector + ' article.active')
         {
-            for (const activeArticle of activeArticles) {
-                activeArticle.classList.remove('active');
+            for (const active_article of active_articles) {
+                active_article.classList.remove('active');
             }
         }
 
         // * [DONE] get 'href' attribute from the clicked link
-        const articleSelector = clickedElement.getAttribute('href');
-        // console.log(articleSelector);
+        const article_selector = clicked_element.getAttribute('href');
+        // console.log(article_selector);
 
         // * [DONE] find the correct article using the selector (value of 'href' attribute), Kodilla
-        const targetArticle = document.querySelector(articleSelector)
-        // console.log(targetArticle);
+        const target_article = document.querySelector(article_selector)
+        // console.log(target_article);
 
         // * [DONE] add class 'active' to the correct article, Kodilla:
-        targetArticle.classList.add('active')
+        target_article.classList.add('active')
 
         // * moje rozwiązanie bez podpowiedzi
         // * [DONE] find the correct article using the selector (value of 'href' attribute) 
         // *        add class 'active' to the correct article
-        // const allArticles = document.querySelectorAll('.posts article')
+        // const all_articles = document.querySelectorAll('.posts article')
         // {
-        //     for (const allArticle of allArticles) {
-        //         if ('#' + allArticle.getAttribute('id') == articleSelector) {
-        //             allArticle.classList.add('active');
+        //     for (const all_article of all_articles) {
+        //         if ('#' + all_article.getAttribute('id') == article_selector) {
+        //             all_article.classList.add('active');
         //         }
         //     }
         // }
     }
 
-    for (const link of links) {
-        // ustawiamy event (-> click) i wywołujemy funkcję
-        // titleClickHandler jest hendlerem eventu
-        link.addEventListener('click', titleClickHandler);  
+   
+
+    // Generowanie listy linków - menu left
+    function generateTitleLinks()
+    {
+        // * remove contents of titleList
+        let title_list = document.querySelector(opt_title_list_selector);
+        // console.log(titleList);
+        title_list.innerHTML = '';
+        // document.querySelector(opt_title_list_selector).innerHTML = '';
+        
+        // * for each article
+        const articles = document.querySelectorAll(opt_all_article_selector + ' article');
+        let html = '';
+        for (const article of articles) {
+            // * get the article id
+            const article_id = article.getAttribute('id');
+            // console.log(article_id);
+            // * find the title element
+            // * get the title from the title element
+            const article_title = article.querySelector(opt_title_selector).innerHTML;
+            // console.log(article_title);
+            // * create HTML of the link
+            // <li><a href="#article-9"><span>Article 9</span></a></li>
+            // * insert link into titleList
+            // metoda 1:
+            html = html + '<li><a href="#' + article_id + '"><span>' + article_title + '</span></a></li>\n';
+            // metoda 2:
+            // title_list.insertAdjacentHTML('beforeend', '<li><a href="#' + article_id + '"><span>' + article_title + '</span></a></li>');
+        }
+        // * insert link into titleList
+        // metoda 1:
+        // console.log(html);
+        title_list.innerHTML = html;
+
+        // lista wszystkich linków lewego menu (class = title) 
+        const links = document.querySelectorAll(opt_title_list_selector + ' a');  
+
+        for (const link of links) {
+            // ustawiamy event (-> click) i wywołujemy funkcję
+            // titleClickHandler jest hendlerem eventu
+            link.addEventListener('click', titleClickHandler);  
+            // console.log(links);
+        }
     }
+
+    generateTitleLinks();
 }
